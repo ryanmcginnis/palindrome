f885e8819246c07edadbf45e478429f7df5bcf43
ryanmcginnis <mcginnisrr@gmail.com>
2015-08-03 20:33:58 -0700

    Update palindrome.py
    Fixed non-alpha input result

--- palindrome.py
+++ palindrome.py
@@ -1,17 +1,28 @@
-import re, os, time
+import re, os, time, string
 
 def palindrome():
-	os.system('clear')
-	word = raw_input("give me a word\n").lower() # remove case
-	word = re.sub(r'\W+', '', word) # remove non-alphanumeric characters
-	reversed = word[::-1] # reverse input
-	os.system('clear')
-	if reversed == word:
-		print "This is a palindrome."
-		time.sleep(0.5)
-		palindrome()
-	else:
-		print "This is not a palindrome."
-		time.sleep(0.5)
-		palindrome()
+	try:
+		os.system('clear')
+		word = str(raw_input("give me a word\n").lower()) # remove case
+		if word in string.letters:
+			word = re.sub(r'\W+', '', word) # remove non-alphanumeric characters
+			reversed = word[::-1] # reverse input
+			os.system('clear')
+			if reversed == word:
+				print "This is a palindrome."
+				time.sleep(0.5)
+				palindrome()
+			else:
+				print "This is not a palindrome."
+				time.sleep(0.5)
+				palindrome()
+		else:
+			os.system('clear')
+			print "Not a word."
+			time.sleep(0.5)
+			palindrome()
+	except KeyboardInterrupt:
+		os.system('clear')
+		print "Quitting...\n"
+		quit()
 palindrome()
